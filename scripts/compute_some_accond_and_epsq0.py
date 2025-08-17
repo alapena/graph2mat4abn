8e8bae5ab9e148f008baa07fec4ce3787172a021
alapena <angel.lapena.lopez@gmail.com>
2025-08-17 16:27:07 +0200

    fixed bugs

--- scripts/compute_some_accond_and_epsq0.py
+++ scripts/compute_some_accond_and_epsq0.py
@@ -19,31 +19,34 @@ warnings.filterwarnings("ignore", message=".*is not a known matrix type key.*")
 def main():
     paths = [
         # hc8:
-        Path("dataset/SHARE_OUTPUTS_2_ATOMS/9b13-4a57-4de9-b863-1b35209370c4"), #train
-        Path("dataset/SHARE_OUTPUTS_8_ATOMS/4ed6-914e-4aa3-923a-53c873f0cc31"), #train
-        Path("dataset/SHARE_OUTPUTS_2_ATOMS/fc1c-6ab6-4c0e-921e-99710e6fe41b"), #val
-        Path("dataset/SHARE_OUTPUTS_8_ATOMS/7b57-1410-4da3-8535-5183ac1f2f61"), #val 
-        Path("dataset/SHARE_OUTPUTS_64_ATOMS/16eb-54f8-42cb-bdb1-7b16f24a650c"), #val
+        # Path("dataset/SHARE_OUTPUTS_2_ATOMS/9b13-4a57-4de9-b863-1b35209370c4"), #train
+        # Path("dataset/SHARE_OUTPUTS_8_ATOMS/4ed6-914e-4aa3-923a-53c873f0cc31"), #train
+        # Path("dataset/SHARE_OUTPUTS_2_ATOMS/fc1c-6ab6-4c0e-921e-99710e6fe41b"), #val
+        # Path("dataset/SHARE_OUTPUTS_8_ATOMS/7b57-1410-4da3-8535-5183ac1f2f61"), #val 
+        # Path("dataset/SHARE_OUTPUTS_64_ATOMS/16eb-54f8-42cb-bdb1-7b16f24a650c"), #val
 
         #hnoc2
-        Path("dataset/SHARE_OUTPUTS_2_ATOMS/9b13-4a57-4de9-b863-1b35209370c4"), #train
-        Path("dataset/SHARE_OUTPUTS_8_ATOMS/e1df-2940-4ada-b9c0-d210a6bb2a19"), #train
-        Path("dataset/SHARE_OUTPUTS_8_ATOMS/4ed6-914e-4aa3-923a-53c873f0cc31"), #train
-        Path("dataset/SHARE_OUTPUTS_64_ATOMS/e46e-c792-412c-99ac-9e20794f7aad"), #train 
-        Path("dataset/SHARE_OUTPUTS_2_ATOMS/504a-71cd-4d25-a04a-b7fa45b92200"), #val
-        Path("dataset/SHARE_OUTPUTS_8_ATOMS/f2b9-d7cc-4f42-9ccc-13dd371d22a5"), #val
-        Path("dataset/SHARE_OUTPUTS_8_ATOMS/72f5-effe-42c4-bc67-12314ba36f5e"), #val 
-        Path("dataset/SHARE_OUTPUTS_64_ATOMS/16eb-54f8-42cb-bdb1-7b16f24a650c"), #val
+        # Path("dataset/SHARE_OUTPUTS_2_ATOMS/9b13-4a57-4de9-b863-1b35209370c4"), #train
+        # Path("dataset/SHARE_OUTPUTS_8_ATOMS/e1df-2940-4ada-b9c0-d210a6bb2a19"), #train
+        # Path("dataset/SHARE_OUTPUTS_8_ATOMS/4ed6-914e-4aa3-923a-53c873f0cc31"), #train
+        # Path("dataset/SHARE_OUTPUTS_64_ATOMS/e46e-c792-412c-99ac-9e20794f7aad"), #train 
+        # Path("dataset/SHARE_OUTPUTS_2_ATOMS/504a-71cd-4d25-a04a-b7fa45b92200"), #val
+        Path("dataset/SHARE_OUTPUTS_8_ATOMS/f2b9-d7cc-4f42-9ccc-13dd371d22a5"), #val ---
+        # Path("dataset/SHARE_OUTPUTS_8_ATOMS/72f5-effe-42c4-bc67-12314ba36f5e"), #val ---
+        # Path("dataset/SHARE_OUTPUTS_64_ATOMS/16eb-54f8-42cb-bdb1-7b16f24a650c"), #val ---
     ]
-    model_dir = Path("results/h_crystalls_8")
+    model_dir = Path("results/h_noc_2")
     filename = "val_best_model.tar"
     split = "val"
+    true_or_pred_idx = 0
+
     n_kpoints_accond = 3
     n_orbs_per_atom = 13
     n_B = 3 #num valence electrons of Boron for DOS calculation
     n_N = 5 #num valence electrons of Nitrogen for DOS calculation
 
 
+    true_or_pred_str = 'true' if true_or_pred_idx == 0 else 'pred'
     savefolder = model_dir.parts[-1] + "_" + split
     savedir = Path("results_ac_cond") / savefolder
     savedir.mkdir(exist_ok=True, parents=True)
@@ -71,7 +74,6 @@ def main():
 
         n_atoms = int(path.parts[-2].split('_')[2])
         structure = path.parts[-1]
-        print(f"Computing structure {n_atoms}atm_{structure}...")
 
         # === Inference ===
         dataset, processor = generate_g2m_dataset_from_paths(config, basis, table, [path], verbose=False)
@@ -87,7 +89,6 @@ def main():
 
         # Plot structure
         file = sisl.get_sile(path / "aiida.fdf")
-        file = sisl.get_sile(path / "aiida.HSX")
         geometry = file.read_geometry()
 
         fig = file.plot.geometry(axes="xyz")
@@ -98,10 +99,12 @@ def main():
         print("Saved structure plot at", filepath)
 
         # Two iterations per sample: first for true, second for pred.
-        for true_or_pred_idx in range(2):
+        # for true_or_pred_idx in range(2): # We comment this because the computations are so long that we prefer to compute the true and pred in paralel.
+        if True:
+            file = sisl.get_sile(path / "aiida.HSX")
             if true_or_pred_idx == 0:
                 # True matrix
-                hamiltonian = file.read_hamiltonian().toscr().tocoo()
+                hamiltonian = file.read_hamiltonian().tocsr().tocoo()
             elif true_or_pred_idx == 1:
                 # Pred matrix
                 hamiltonian = h_pred.tocsr().tocoo()
@@ -117,23 +120,30 @@ def main():
             # Compute AC Conductivity
 
             # Load true DOS
+            directory_dos = Path("results_dos") / savefolder
             struct_name = f"{n_atoms}atm_" + structure
-            filename = split + "_" + struct_name + "_dos_mesh30_true.npz"
-            directory_dos = Path("results_dos")
-            filename = directory_dos / filename
-            loaded_file = np.load(filename)
+       
+            pattern = f"{split}_{struct_name}_dos_mesh*_{true_or_pred_str}.npz"
+            matching_files = list(directory_dos.glob(pattern))
+            if matching_files:
+                filepath = matching_files[-1] # Load the last match (if any)
+                print(f"Loading file {filepath}")
+            else:
+                raise ValueError(f"No matching file found in {directory_dos} with pattern {pattern}.")
+
+            loaded_file = np.load(filepath)
             energies = loaded_file["energies"]
             dos_true = loaded_file["dos"]
-            print("DOS data extracted from", filename)
+            print("DOS data extracted from", filepath)
 
-            n_orbs_supercell = read_orbital_count(path / "aiida.out")
+            n_orbs_supercell = read_orbital_count(path / "aiida.ORB_INDX")
             n_cells = n_orbs_supercell//n_orbs_per_atom//int(n_atoms)
 
             lind = tb.Lindhard(ham_cell, overlap=overlap_cell)
             e_min = 0
             e_fermi = get_fermi_energy(energies, dos_true, n_orbs_per_atom*int(n_atoms)*n_cells, (3*n_B + 5*n_N)*n_cells, ShowPlot=False)
             # e_max = np.min([np.max(energies), e_fermi+20])
-            e_max = 20
+            e_max = 18
 
             cfg = lind.config
             cfg.prefix       = "ac_cond"   # basename for saved results (optional)
@@ -160,6 +170,7 @@ def main():
             if true_or_pred_idx == 1:
                 filepath = savedir / f"{split}_{n_atoms}atm_{structure}_accond_pred.npz"
             np.savez(filepath, path=str(path), omegas=omegas, ac_cond=ac_cond, time=timer._time_usage["ac_cond"])
+            print(f"Saved AC Conductivity of structure {struct_name} at {filepath}")
 
             timer = tb.Timer()
             timer.tic("diel_demo")
@@ -171,6 +182,7 @@ def main():
             if true_or_pred_idx == 1:
                 filepath = savedir / f"{split}_{n_atoms}atm_{structure}_epsq0_pred.npz"
             np.savez(filepath, path=str(path), eps_q0=eps_q0, time=timer._time_usage["diel_demo"])
+            print(f"Saved dielectric function of structure {struct_name} at {filepath}")
 
 
 
